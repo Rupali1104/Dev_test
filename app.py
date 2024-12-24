dddd1a0d95716d77d76bf0f3c290d145b08532fd
Rupali1104 <rupaliii739@gmail.com>
2024-12-24 15:57:33 +0530

    Program updated

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello world")
+def calculate_percentage(obtained_marks, total_marks):
+    percentage = (obtained_marks / total_marks) * 100
+    return percentage
+
+total_marks = float(input("Enter the total marks: "))
+obtained_marks = float(input("Enter the obtained marks: "))
+
+percentage = calculate_percentage(obtained_marks, total_marks)
+
+print(f"Percentage: {percentage:.2f}%")
+
