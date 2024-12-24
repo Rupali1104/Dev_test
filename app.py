460e0555b2f5b75e9a3d96aa37500029ef243542
Rupali1104 <rupaliii739@gmail.com>
2024-12-24 16:02:32 +0530

    docker updated

--- app.py
+++ app.py
@@ -2,4 +2,3 @@ total_marks = float(input("Enter the total marks: "))
 obtained_marks = float(input("Enter the obtained marks: "))
 percentage = (obtained_marks / total_marks) * 100
 print(f"Percentage: {percentage:.2f}%")
-50
